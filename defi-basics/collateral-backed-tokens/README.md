56406b300d2a1a6280070ad8ad750e8d6cdcad92
Ugo Plouviez <ugo.plouviez@gmail.com>
2021-04-23 06:53:27 +0200

    Add comments to colleral-backed-token

--- defi-basics/collateral-backed-tokens/README.md
+++ defi-basics/collateral-backed-tokens/README.md
@@ -1,3 +1,3 @@
 # Collateral backed token
 
-Example of a smart contract that is sending a collateral backed token (CBT) for some ERC20 token. The initial amount of ERC20 token can be redeemed when the user decides to withdraw his funds. The matching CBT is then burnt.
+Example of a smart contract that is sending a collateral backed token (CBT) in exchange of some ERC20 tokens. The initial amount of ERC20 token can be redeemed when the user decides to withdraw his funds. The matching amount of CBT is then burnt. The ratio between the initial ERC20 token and the CBT token is here set to 1 but it could be modified to match a real case scenario.
--- defi-basics/collateral-backed-tokens/contracts/CollateralBackedToken.sol
+++ defi-basics/collateral-backed-tokens/contracts/CollateralBackedToken.sol
@@ -11,11 +11,14 @@ contract CollateralBackedToken is ERC20 {
         collateral = IERC20(collateral);
     }
 
+    // send the amount of ERC20 token to this contract's address, then mint the matching amount of CBT for the sender.
     function deposit(uint collateralAmount) external {
         collateral.transferFrom(msg.sender, address(this), collateralAmount);
         _mint(msg.sender, collateralAmount * price);
     }
 
+    // the withdraw will check that the balance is enough, burn the matching amount of CBT token and finally transfer back 
+    // the original amount of collateralized tokens.
     function withdraw(uint tokenAmount) external {
         require(balanceOf(msg.sender) >= tokenAmount, 'Balance too low');
         _burn(msg.sender, tokenAmount);
